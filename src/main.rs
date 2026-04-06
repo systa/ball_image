2457cf1be6e3a9e9202f48fba770058d7cecbd37
systa <kari.systa@tuni.fi>
2026-04-06 12:50:56 +0300

    Now with two different projections

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,10 @@ struct Args {
     #[arg(long, default_value_t = false)]
     transparent: bool,
 
+    /// Transparent outside circle (default is black)
+    #[arg(long, default_value_t = false)]
+    straight: bool,
+
     /// Output width (defaults to input width)
     #[arg(long)]
     width: Option<u32>,
@@ -54,9 +58,13 @@ fn main() -> Result<()> {
     let out_h = args.height.unwrap_or(in_h);
     let src = img.to_rgba8();
 
-    let output: RgbaImage = ImageBuffer::from_fn(out_w, out_h, |x, y| {
-        sample_sphere(x, y, out_w, out_h, &src, args.strength, args.transparent)
-    });
+    let output: RgbaImage = if args.straight { ImageBuffer::from_fn(out_w, out_h, |x, y| {
+        sample_sphere1(x, y, out_w, out_h, &src, args.strength, args.transparent)
+    })} else {
+        ImageBuffer::from_fn(out_w, out_h, |x, y| {
+            sample_sphere(x, y, out_w, out_h, &src, args.strength, args.transparent)
+        })
+    };
 
     output
         .save_with_format(&args.output, image::ImageFormat::from_path(&args.output).with_context(|| {
@@ -68,9 +76,54 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-static mut max_y: u32 = 0;
-static mut max_x: u32 = 0;
 fn sample_sphere(
+    x: u32,
+    y: u32,
+    width: u32,
+    height: u32,
+    src: &RgbaImage,
+    strength: f32,
+    transparent_outside: bool,
+) -> Rgba<u8> {
+    let cx = width as f32 / 2.0;
+    let cy = height as f32 / 2.0;
+    let radius = cx.min(cy);
+    let dx = x as f32 - cx;
+    let dy = y as f32 - cy;
+    let r = (dx * dx + dy * dy).sqrt();
+
+    if r > radius {
+        return if transparent_outside {
+            Rgba([0, 0, 0, 0])
+        } else {
+            Rgba([0, 0, 0, 255])
+        };
+    }
+    let sin_t = (r / radius).clamp(0.0, 1.0).powf(strength.max(0.001));
+//    let sin_x = (dx / radius).signum() * (dx / radius).abs().powf(strength.max(0.001));
+//    let sin_y = (dy / radius).signum() * (dy / radius).abs().powf(strength.max(0.001));
+
+    let sx = dx*sin_t.asin() / radius;
+    let sy = dy*sin_t.asin() / radius;
+
+    let rwidth = radius;// (width as f32 / 2.0);
+    let rheight = radius; // (height as f32 / 2.0);
+    let sample_x0 = ((cx as f32 + sx*rwidth/PI*2.0) as u32);
+    let sample_x = sample_x0.clamp(1, src.width() - 2);
+    let sample_y0 = ((cy as f32 + sy*rheight/PI*2.0) as u32 ).clamp(1, src.height() - 2);
+    let sample_y = sample_y0.clamp(1, src.height() - 2);
+//    if ((x == 2524 && y == 1516) || (x == 3524 && y == 1516) || (x == 2524 && y == 2516) || (x == 3524 && y == 2516)) {
+//    if (y == 20160) {
+    if (x == 2016 && (y == 2016 || y == 1008)) {
+        println!(
+            "x: {}, y: {}, dx: {:.2}, dy: {:.2}, r: {:.2}, radius: {:.2}, sin_t: {:.4}, sx: {:.4}, sy: {:.4}, sample_x0: {}, sample_x: {}, sample_y0: {}, sample_y: {}",
+            x, y, dx, dy, r, radius, sin_t , sx, sy, sample_x0, sample_x, sample_y0, sample_y
+        );
+    }
+    *src.get_pixel(sample_x, sample_y)
+}
+
+fn sample_sphere1(
     x: u32,
     y: u32,
     width: u32,
