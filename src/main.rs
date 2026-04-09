5cbf4daaa15c3a41b6583ea978556c5a7c1de487
Kari Systä <kari.systa@tuni.fi>
2026-04-09 21:49:05 +0300

    Fixed  middle scaling and added sharpening

--- src/main.rs
+++ src/main.rs
@@ -26,10 +26,18 @@ struct Args {
     #[arg(long, default_value_t = false)]
     transparent: bool,
 
-    /// Alternative projektion to keep horizontal and vertical are kept (default is false)
+    /// Alternative projektion to keep horizontal and vertical lines (default is false)
     #[arg(long, default_value_t = false)]
     keepbox: bool,
 
+    /// Up-scale middle part to avoid artifacts
+    #[arg(long, default_value_t = false)]
+    expand: bool,
+    
+    /// Sharpen middle (after up-scaling)
+    #[arg(long, default_value_t = false)]
+    sharpen: bool,
+    
     /// Output width (defaults to input width)
     #[arg(long)]
     width: Option<u32>,
@@ -74,7 +82,7 @@ fn main() -> Result<()> {
     let scale_factor = 4u32;
     let sub_image = scale::bl_resize(&sub_image, center_h * scale_factor, center_w * scale_factor);
 
-    /*
+    
     println!(
         "Extracted center {}x{} region at ({}, {}) and scaled it to {}x{}",
         center_w,
@@ -84,17 +92,21 @@ fn main() -> Result<()> {
         sub_image.width(),
         sub_image.height()
     );
-    */
+    
 
-    let output: RgbaImage = if args.keepbox { ImageBuffer::from_fn(out_w, out_h, |x, y| {
+    let mut output: RgbaImage = if args.keepbox { ImageBuffer::from_fn(out_w, out_h, |x, y| {
         sample_sphere1(x, y, out_w, out_h, &src, args.strength, args.transparent)
     })} else {
         ImageBuffer::from_fn(out_w, out_h, |x, y| {
-            sample_sphere(x, y, out_w, out_h, &src, args.strength, args.transparent, left, top, left + center_w, top + center_h, &sub_image)
+            sample_sphere(x, y, out_w, out_h, &src, args.strength, args.transparent, args.expand, left, top, left + center_w, top + center_h, &sub_image)
         })
     };
 
-    output
+    if args.sharpen {
+    // run simple sharpening to the interpolated center
+       sharpen(&mut output, left, top, left + center_w, top + center_h)
+    };
+    output	     
         .save_with_format(&args.output, image::ImageFormat::from_path(&args.output).with_context(|| {
             format!("Cannot infer output image format from '{}'", args.output.display())
         })?)
@@ -102,6 +114,31 @@ fn main() -> Result<()> {
 
     println!("Saved: {}", args.output.display());
     Ok(())
+}
+
+
+fn sharpen(
+ src: &mut RgbaImage,
+ x1 : u32,
+ y1 : u32,
+ x2 : u32,
+ y2 : u32
+) {
+  let copy: RgbaImage = ImageBuffer::from_fn(src.width(), src.height(), | x, y | {*src.get_pixel(x,y)});
+  for i in x1+1..x2-1 {
+      for j in y1+1..y2-1 {
+      	  let n = copy.get_pixel(i, j-1);
+      	  let w = copy.get_pixel(i-1, j);
+      	  let c = copy.get_pixel(i, j);
+      	  let e = copy.get_pixel(i+1, j);
+      	  let s = copy.get_pixel(i, j);
+      	  let mut new_pix = *src.get_pixel(i,j);
+      	  for k in 0..3 {
+      	    new_pix[k] = (5*(c[k] as i32) - n[k] as i32 - w[k] as i32 - e[k] as i32 - s[k] as i32).clamp(0,255) as u8;
+      	  }
+	  (*src).put_pixel(i,j,new_pix);
+      }
+  }
 }
 
 fn sample_sphere(
@@ -112,6 +149,7 @@ fn sample_sphere(
     src: &RgbaImage,
     strength: f32,
     transparent_outside: bool,
+    expand_middle: bool,
     left: u32,
     top: u32,
     right: u32,
@@ -143,25 +181,39 @@ fn sample_sphere(
 
     let rwidth = radius;// (width as f32 / 2.0);
     let rheight = radius; // (height as f32 / 2.0);
-    let sample_x0 = ((cx as f32 + sx*rwidth/PI*2.2) / scalex) as u32;
-    let sample_x = sample_x0.clamp(1, src.width() - 2);
-    let sample_y0 = ((cy as f32 + sy*rheight/PI*2.2) / scaley) as u32;
-    let sample_y = sample_y0.clamp(1, src.height() - 2);
+    let sample_x0 = (cx as f32 + sx*rwidth/PI*2.2) / scalex;
+    let sample_x = (sample_x0 as u32).clamp(1, src.width() - 2);
+    let sample_y0 = (cy as f32 + sy*rheight/PI*2.2) / scaley;
+    let sample_y = (sample_y0 as u32).clamp(1, src.height() - 2);
 //    if ((x == 2524 && y == 1516) || (x == 3524 && y == 1516) || (x == 2524 && y == 2516) || (x == 3524 && y == 2516)) {
 //    if (y == 20160) {
-/*
-    if dx.abs() < 9.0 && dy.abs() < 1.0 {
-        println!(
+
+//    if dx.abs() < 9.0 && dy.abs() < 1.0 {
+    if y == height / 2 {  
+/*        println!(
             "x: {}, y: {}, dx: {:.2}, dy: {:.2}, r: {:.2}, radius: {:.2}, sin_t: {:.4}, sx: {:.4}, sy: {:.4}, sample_x0: {}, sample_x: {}, sample_y0: {}, sample_y: {}",
             x, y, dx, dy, r, radius, sin_t , sx, sy, sample_x0, sample_x, sample_y0, sample_y
-        );
-    }
 */
-    if sample_x >= left && sample_x < right && sample_y >= top && sample_y < bottom {
-        let sub_x = (sample_x - left)*4;
-        let sub_y = (sample_y - top)*4;
+/*
+        println!(
+            "x: {}, y: {}, sample_x0: {}, sample_x: {}, sample_y0: {}, sample_y: {}",
+            x, y, sample_x0, sample_x, sample_y0, sample_y
+        );
+*/
+    }
+
+
+
+    if expand_middle && sample_x >= left && sample_x < right && sample_y >= top && sample_y < bottom {
+        let sub_x = ((sample_x0 - left as f32)*4.0) as u32;
+        let sub_y = ((sample_y0  - top as f32)*4.0) as u32;
+/*	if sample_x == cx as u32 && sample_y == cy as u32 {
+      	    println!("in the middle ({},{}), sub=({},{}) left={}, top={}, right={}, bottom={}", sample_x, sample_y, sub_x, sub_y, left, top, right, bottom);
+    	} */
         return *sub_image.get_pixel(sub_x, sub_y);
     }
+
+
     *src.get_pixel(sample_x, sample_y)
 }
 
